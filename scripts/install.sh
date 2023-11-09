df9120d2b3a1187b1e89e4e4c03a63e5c66ae628
Daniel Sales <danisales.es@gmail.com>
2023-11-09 12:48:46 +0100

    fix: instalación

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,32 +11,41 @@ if [ -z "$NODE_PATH" ]; then
 fi
 echo "NODE_PATH: $NODE_PATH"
 
+echo "NODE VERSION: $(sudo node -v)"
+
 PACKAGE="wikijs-api"
 echo "PACKAGE: $PACKAGE"
 
 echo "Cloning $PACKAGE ..."
-sudo rm -rf "$NODE_PATH/$PACKAGE"
-sudo git clone "https://github.com/ByDSA/wikijs-api" "$NODE_PATH/$PACKAGE"
+TMP_DIR=".tmp-$PACKAGE"
+sudo rm -rf "$TMP_DIR"
 
-if [ -L "$NODE_PATH/$PACKAGE" ]; then
-  ROOT_PACKAGE=$(readlink "$NODE_PATH/$PACKAGE")
-  echo "ROOT_PACKAGE: $ROOT_PACKAGE"
-  sudo rm -rf "$NODE_PATH/$PACKAGE"
-  echo "Moving $ROOT_PACKAGE to $NODE_PATH/$PACKAGE"
-  sudo mv "$ROOT_PACKAGE" "$NODE_PATH/$PACKAGE"
-  echo "Changing permissions of $NODE_PATH/$PACKAGE"
-  sudo chmod -R 755 "$NODE_PATH/$PACKAGE"
+sudo git clone "https://github.com/ByDSA/wikijs-api" "$TMP_DIR"
+
+if [ -L "$TMP_DIR" ]; then
+  ROOT_PACKAGE=$(readlink "$TMP_DIR")
+  echo "ROOT_PACKAGE: $TMP_DIR"
+  sudo rm -rf "$TMP_DIR"
+  echo "Moving $ROOT_PACKAGE to $TMP_DIR"
+  sudo mv "$ROOT_PACKAGE" "$TMP_DIR"
+  echo "Changing permissions of $TMP_DIR"
+  sudo chmod -R 755 "$TMP_DIR"
 fi
 echo "Installing dependencies ..."
-cd "$NODE_PATH/$PACKAGE"
+cd "$TMP_DIR"
 sudo npm i
 echo "Building ..."
 sudo npm run build
+cd ..
+
+FINAL_DIR="$NODE_PATH/$PACKAGE"
+sudo rm -rf "$FINAL_DIR"
+sudo mv "$TMP_DIR" "$FINAL_DIR"
 
 BIN_OUT=/usr/local/bin/wikijs
 echo "Creating symlink for $BIN_OUT ..."
 sudo rm -rf "$BIN_OUT"
-sudo ln -s "$NODE_PATH/$PACKAGE/dist/cli/bin.js" "$BIN_OUT"
+sudo ln -s "$FINAL_DIR/dist/cli/bin.js" "$BIN_OUT"
 echo "Changing permissions of $BIN_OUT ..."
 sudo chmod +x "$BIN_OUT"
 
